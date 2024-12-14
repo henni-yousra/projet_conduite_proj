9ddc69f86d4aac7674bb60e831f0ae55c00ad10a
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2024-12-14 17:43:25 +0100

    tests for task components

--- frontend/src/app/task-dialog/task-dialog.component.spec.ts
+++ frontend/src/app/task-dialog/task-dialog.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskDialogComponent } from './task-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TaskDialogComponent', () => {
   let component: TaskDialogComponent;
@@ -8,7 +10,9 @@ describe('TaskDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskDialogComponent]
+      imports: [TaskDialogComponent, MatDialogModule, BrowserAnimationsModule],
+      providers: [{provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: []}]
     })
     .compileComponents();
 

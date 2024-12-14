4d5a1eee2b35ea812bdb9707a6667e37dd5ac8a1
cdrezen <corentin.drezen@etu.u-bordeaux.fr>
2024-12-14 17:27:50 +0100

    default tests for issue components

--- frontend/src/app/issues/issues.component.spec.ts
+++ frontend/src/app/issues/issues.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IssuesComponent } from './issues.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../app-routing.module';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('IssuesComponent', () => {
   let component: IssuesComponent;
@@ -8,7 +11,8 @@ describe('IssuesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [IssuesComponent]
+      imports: [IssuesComponent],
+      providers: [provideHttpClient(), provideRouter(routes)]
     })
     .compileComponents();
 
